683c99b689483d984839888566915f7fd1cb82bc
oneinchlunch <amir1mrkonjic@gmail.com>
2021-01-03 02:24:26 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent {
+  title = 'portofolio';
+  config: any;
+  fullpage_api: any;
+  
+  constructor(){
+    this.config = {
+      anchors:[],
+      menu: '#menu',
+      navigation: true,
+
+      afterResize: () => {
+        console.log("AfterResize");
+      },
+
+      afterLoad: (origin, destination, direction) => {
+        console.log(origin.index);
+      }
+    };
+  }
+
+  getRef(fullPageRef) {
+    this.fullpage_api = fullPageRef;
+  }
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AngularFullpageModule } from '@fullpage/angular-fullpage';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFullpageModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
